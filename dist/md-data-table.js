c388d6986e0e339489afb6ce202a56dd27a96e00
Florian Maul <f.maul@fme.de>
2016-02-24 14:11:23 +0000

    Fixing async/lazy label changes with ng $translate

--- dist/md-data-table.js
+++ dist/md-data-table.js
@@ -1219,111 +1219,115 @@ function mdTable() {
 angular.module('md.data.table').directive('mdTablePagination', mdTablePagination);
 
 function mdTablePagination() {
-  
+
   function compile(tElement) {
     tElement.addClass('md-table-pagination');
   }
-  
+
   function Controller($attrs, $scope) {
     var self = this;
-    
-    self.$label = angular.extend({
-      page: 'Page:',
-      rowsPerPage: 'Rows per page:',
-      of: 'of'
-    }, $scope.$eval(self.label) || {});
-    
+
+    function updateTranslations() {
+      self.$label = angular.extend({
+        page: 'Page:',
+        rowsPerPage: 'Rows per page:',
+        of: 'of'
+      }, $scope.$eval(self.label) || {});
+    }
+    updateTranslations();
+    $scope.$watch('$pagination.label', updateTranslations);
+
     function isPositive(number) {
       return number > 0;
     }
-    
+
     function isZero(number) {
       return number === 0 || number === '0';
     }
-    
+
     self.disableNext = function () {
       return isZero(self.limit) || !self.hasNext();
     };
-    
+
     self.first = function () {
       self.page = 1;
       self.onPaginationChange();
     };
-    
+
     self.hasNext = function () {
       return self.page * self.limit < self.total;
     };
-    
+
     self.hasPrevious = function () {
       return self.page > 1;
     };
-    
+
     self.last = function () {
       self.page = self.pages();
       self.onPaginationChange();
     };
-    
+
     self.max = function () {
       return self.hasNext() ? self.page * self.limit : self.total;
     };
-    
+
     self.min = function () {
       return self.page * self.limit - self.limit;
     };
-    
+
     self.next = function () {
       self.page++;
       self.onPaginationChange();
     };
-    
+
     self.onPaginationChange = function () {
       if(angular.isFunction(self.onPaginate)) {
         self.onPaginate(self.page, self.limit);
       }
     };
-    
+
     self.pages = function () {
       return Math.ceil(self.total / (isZero(self.limit) ? 1 : self.limit));
     };
-    
+
     self.previous = function () {
       self.page--;
       self.onPaginationChange();
     };
-    
+
     self.range = function (total) {
       return new Array(isFinite(total) && isPositive(total) ? total : 1);
     };
-    
+
     self.showBoundaryLinks = function () {
       if($attrs.hasOwnProperty('mdBoundaryLinks') && $attrs.mdBoundaryLinks === '') {
         return true;
       }
-      
+
       return self.boundaryLinks;
     };
-    
+
     self.showPageSelect = function () {
       if($attrs.hasOwnProperty('mdPageSelect') && $attrs.mdPageSelect === '') {
         return true;
       }
-      
+
       return self.pageSelect;
     };
-    
+
     $scope.$watch('$pagination.limit', function (newValue, oldValue) {
       if(newValue === oldValue) {
         return;
       }
-      
+
       // find closest page from previous min
       self.page = Math.floor(((self.page * oldValue - oldValue) + newValue) / (isZero(newValue) ? 1 : newValue));
       self.onPaginationChange();
     });
   }
-  
+
   Controller.$inject = ['$attrs', '$scope'];
-  
+
   return {
     bindToController: {
       boundaryLinks: '=?mdBoundaryLinks',
@@ -1343,6 +1347,7 @@ function mdTablePagination() {
     templateUrl: 'md-table-pagination.html'
   };
 }
+
 
 angular.module('md.data.table').directive('mdTableProgress', mdTableProgress);
 
